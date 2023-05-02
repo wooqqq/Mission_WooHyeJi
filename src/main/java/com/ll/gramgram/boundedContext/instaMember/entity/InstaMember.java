6ddba749bdc41adcae3eedd7152567efd47334e4
깃wooqqq <sge0225@naver.com>
2023-05-02 14:29:37 +0900

    feat : event 생성. 스냅샷과 인스타계정 관련 수치 갱신을 이벤트를 통해서 수행

--- src/main/java/com/ll/gramgram/boundedContext/instaMember/entity/InstaMember.java
+++ src/main/java/com/ll/gramgram/boundedContext/instaMember/entity/InstaMember.java
@@ -1,11 +1,11 @@
 package com.ll.gramgram.boundedContext.instaMember.entity;
 
-import com.ll.gramgram.base.baseEntity.BaseEntity;
-import com.ll.gramgram.boundedContext.instaMember.service.InstaMemberService;
 import com.ll.gramgram.boundedContext.likeablePerson.entity.LikeablePerson;
-import groovy.util.ObservableList;
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 import lombok.experimental.SuperBuilder;
 import org.hibernate.annotations.LazyCollection;
 import org.hibernate.annotations.LazyCollectionOption;
@@ -33,11 +33,6 @@ public class InstaMember extends InstaMemberBase {
     @LazyCollection(LazyCollectionOption.EXTRA)
     @Builder.Default // @Builder 가 있으면 ` = new ArrayList<>();` 가 작동하지 않는다. 그래서 이걸 붙여야 한다.
     private List<LikeablePerson> toLikeablePeople = new ArrayList<>();
-
-    @OneToMany(mappedBy = "instaMember", cascade = {CascadeType.ALL})
-    @OrderBy("id desc") // 정렬
-    @Builder.Default
-    private List<InstaMemberSnapshot> instaMemberSnapshots = new ArrayList<>();
 
     public void addFromLikeablePerson(LikeablePerson likeablePerson) {
         fromLikeablePeople.add(0, likeablePerson);
@@ -69,8 +64,6 @@ public class InstaMember extends InstaMemberBase {
         if (gender.equals("M") && attractiveTypeCode == 1) likesCountByGenderManAndAttractiveTypeCode1++;
         if (gender.equals("M") && attractiveTypeCode == 2) likesCountByGenderManAndAttractiveTypeCode2++;
         if (gender.equals("M") && attractiveTypeCode == 3) likesCountByGenderManAndAttractiveTypeCode3++;
-
-        saveSnapshot();
     }
 
     public void decreaseLikesCount(String gender, int attractiveTypeCode) {
@@ -80,44 +73,26 @@ public class InstaMember extends InstaMemberBase {
         if (gender.equals("M") && attractiveTypeCode == 1) likesCountByGenderManAndAttractiveTypeCode1--;
         if (gender.equals("M") && attractiveTypeCode == 2) likesCountByGenderManAndAttractiveTypeCode2--;
         if (gender.equals("M") && attractiveTypeCode == 3) likesCountByGenderManAndAttractiveTypeCode3--;
-
-        saveSnapshot();
     }
 
-    public boolean updateGender(String gender) {
-        if (gender.equals(this.gender)) return false;
-
-        boolean oldIsNull = this.gender == null;
-
-        String oldGender = this.gender;
-
-        getFromLikeablePeople()
-                .forEach(likeablePerson -> {
-                    // 내가 좋아하는 사람 불러오기
-                    InstaMember toInstaMember = likeablePerson.getToInstaMember();
-                    toInstaMember.decreaseLikesCount(oldGender, likeablePerson.getAttractiveTypeCode());
-                    toInstaMember.increaseLikesCount(gender, likeablePerson.getAttractiveTypeCode());
-                });
-
+    public void updateGender(String gender) {
         this.gender = gender;
-
-        if (!oldIsNull) saveSnapshot();
-
-        return true;
     }
 
-    public void saveSnapshot() {
-        InstaMemberSnapshot instaMemberSnapshot = InstaMemberSnapshot.builder()
-                .instaMember(this)
+    public InstaMemberSnapshot snapshot(String eventTypeCode) {
+        return InstaMemberSnapshot
+                .builder()
+                .eventTypeCode(eventTypeCode)
                 .username(username)
-                .likesCountByGenderWomanAndAttractiveTypeCode1(likesCountByGenderWomanAndAttractiveTypeCode1)
-                .likesCountByGenderWomanAndAttractiveTypeCode2(likesCountByGenderWomanAndAttractiveTypeCode2)
-                .likesCountByGenderWomanAndAttractiveTypeCode3(likesCountByGenderWomanAndAttractiveTypeCode3)
+                .instaMember(this)
+                .gender(gender)
                 .likesCountByGenderManAndAttractiveTypeCode1(likesCountByGenderManAndAttractiveTypeCode1)
                 .likesCountByGenderManAndAttractiveTypeCode2(likesCountByGenderManAndAttractiveTypeCode2)
                 .likesCountByGenderManAndAttractiveTypeCode3(likesCountByGenderManAndAttractiveTypeCode3)
+                .likesCountByGenderWomanAndAttractiveTypeCode1(likesCountByGenderWomanAndAttractiveTypeCode1)
+                .likesCountByGenderWomanAndAttractiveTypeCode2(likesCountByGenderWomanAndAttractiveTypeCode2)
+                .likesCountByGenderWomanAndAttractiveTypeCode3(likesCountByGenderWomanAndAttractiveTypeCode3)
                 .build();
 
-        instaMemberSnapshots.add(instaMemberSnapshot);
     }
 }
