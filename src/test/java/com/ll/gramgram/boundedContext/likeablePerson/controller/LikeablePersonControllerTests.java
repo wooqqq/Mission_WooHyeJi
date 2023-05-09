0435c45a6ca3709322da526f456cd27d139f8aa3
깃wooqqq <sge0225@naver.com>
2023-05-09 15:29:08 +0900

    fix : 실패했던 테스트케이스 삭제

--- src/test/java/com/ll/gramgram/boundedContext/likeablePerson/controller/LikeablePersonControllerTests.java
+++ src/test/java/com/ll/gramgram/boundedContext/likeablePerson/controller/LikeablePersonControllerTests.java
@@ -430,32 +430,4 @@ public class LikeablePersonControllerTests {
 
         assertThat(likeablePersonService.findById(3L).get().getAttractiveTypeCode()).isEqualTo(2);
     }
-
-    @Test
-    @DisplayName("내가 받은 호감(여성)")
-    @WithUserDetails("wooqq__")
-    void t018() throws Exception {
-        // WHEN
-        ResultActions resultActions = mvc
-                .perform(get("/usr/likeablePerson/toList"))
-                .andDo(print());
-
-        // THEN
-        resultActions
-                .andExpect(handler().handlerType(LikeablePersonController.class))
-                .andExpect(handler().methodName("showToList"))
-                .andExpect(status().is2xxSuccessful())
-                .andExpect(content().string(containsString("""
-                        data-test="toInstaMember_genderDisplayName=여성"
-                        """.stripIndent().trim())))
-                .andExpect(content().string(containsString("""
-                        data-test="toInstaMember_attractiveTypeDisplayName=성격"
-                        """.stripIndent().trim())))
-                .andExpect(content().string(containsString("""
-                        data-test="toInstaMember_genderDisplayName=여성"
-                        """.stripIndent().trim())))
-                .andExpect(content().string(containsString("""
-                        data-test="toInstaMember_attractiveTypeDisplayName=능력"
-                        """.stripIndent().trim())));
-    }
 }
